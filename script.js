a69718fc63b2aa8bc612ca5536c0a041e92079c5
Satyam Kumar <99055360+AirSatyam07@users.noreply.github.com>
2024-11-17 11:22:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed= new Typed(".multiple-text",{
+    strings: ["FrontEnd Developer ", "Backend Developer" , "ML Enthusiast" , "Electrical Engineer by Degree"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay: 1000,
+    loop:true
+})
